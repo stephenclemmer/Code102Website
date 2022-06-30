ea2b341bd3d609798d70d6d1017233438f12000d
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-06-29 22:05:27 -0700

    Added a null result

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let usersPokemon = prompt("Guess my favorite Pokemon?")
-
+if (usersPokemon == null){
+    console.log("What? You don't want to guess?! Okay, you're officially no fun.");
+}
 if (usersPokemon.toLowerCase() == "pikachu"){
     console.log("You got it! I find pikachu electrifying!");
 } else {
