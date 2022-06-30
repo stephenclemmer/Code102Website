e3c6d8d95029d39df7772bba2ca6a03f29fa143d
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-06-29 21:42:55 -0700

    working javascript guessing game

---
+++ app.js
@@ -0,0 +1,7 @@
+let usersPokemon = prompt("Guess my favorite Pokemon?")
+
+if (usersPokemon.toLowerCase() == "pikachu"){
+    console.log("You got it! I find pikachu electrifying!");
+} else {
+    console.log("That's a great pokemon, but that's not my favorite. Try again! (Hint: Sometimes they are sweet and sometimes they are downright shocking.");
+}
