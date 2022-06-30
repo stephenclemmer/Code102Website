33f8abe316372167730746d2d073208bd6c1de51
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-06-29 22:12:48 -0700

    added else if to the logic flow because I was erroring by toLowerCase null

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let usersPokemon = prompt("Guess my favorite Pokemon?")
-if (usersPokemon == null){
+if (usersPokemon == null) {
     console.log("What? You don't want to guess?! Okay, you're officially no fun.");
-}
-if (usersPokemon.toLowerCase() == "pikachu"){
+} else if (usersPokemon.toLowerCase() == "pikachu") {
     console.log("You got it! I find pikachu electrifying!");
 } else {
     console.log("That's a great pokemon, but that's not my favorite. Try again! (Hint: Sometimes they are sweet and sometimes they are downright shocking.");
